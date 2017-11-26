8625a79b9b0bfef87d5906044f1fd7749e0c5328
rozetko <rozetko@corpglory.com>
2017-11-26 14:21:18 +0200

    Changed time prettify method

--- src/components/BoardAnimal.vue
+++ src/components/BoardAnimal.vue
@@ -2,7 +2,7 @@
 <Billboard>
   <p>You exposed <span class="measure">{{measure}} µg/m<sup>3</sup> {{measureName}}</span>  while driving</p>
   <div class="decoration"></div>
-  <p> a <span class="animal">{{animal}}</span> will die in <span class="life-exp">{{lifeExp}} hours</span></p>
+  <p> a <span class="animal">{{animal}}</span> will die in <span class="life-exp">{{pretty(lifeExp)}}</span></p>
   <p class="board-resume">Animals are not guilty </p>
 </Billboard>
 </template>
@@ -24,29 +24,34 @@ export default {
     let infos = animal['info'];
     let info = infos[Math.floor(Math.random()*infos.length)];
 
-    let pretty = function(duration) {
-        var milliseconds = parseInt((duration%1000)/100)
-            , seconds = parseInt((duration/1000)%60)
-            , minutes = parseInt((duration/(1000*60))%60)
-            , hours = parseInt((duration/(1000*60*60))%24);
-
-        hours = (hours < 10) ? "0" + hours : hours;
-        minutes = (minutes < 10) ? "0" + minutes : minutes;
-        seconds = (seconds < 10) ? "0" + seconds : seconds;
-        if(hours === 0) return minutes;
-        return hours;
-    }
-
     return {
       measure: info['concentration'],
       measureName: info['measure'],
       animal: animal['animal'],
-      lifeExp: pretty(info['lifeexp'])
+      lifeExp: info['lifeexp']
     }
   },
   methods: {
     subscript: function(digit) {
       return digit.replace(/(\d+)/g, '<sub>$1</sub>');
+    },
+    pretty: function(duration) {
+      var milliseconds = parseInt((duration%1000)/100)
+        , seconds = parseInt((duration/1000)%60)
+        , minutes = parseInt((duration/(1000*60))%60)
+        , hours = parseInt((duration/(1000*60*60))%24);
+
+      hours = (hours < 10) ? "0" + hours : hours;
+      minutes = (minutes < 10) ? "0" + minutes : minutes;
+      seconds = (seconds < 10) ? "0" + seconds : seconds;
+      if(Number(hours) === 0) {
+        if(Number(minutes) !== 0) {
+          return `${minutes} minutes`;
+        } else {
+          return `${seconds} seconds`;
+        }
+      }
+      return `${hours} hours`;
     }
   },
   components: {
